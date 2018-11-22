ba17cee4b0ae07557f0ee1063747d488c37a3214
Norbert Benczúr <nbe@vizrt.com>
2018-11-22 13:02:04 +0100

    Changed update submodule script

--- update_submodules.sh
+++ update_submodules.sh
@@ -6,5 +6,5 @@ git fetch
 git log --pretty=oneline --abbrev-commit --decorate HEAD..origin/master
 cd ../../
 
-# Update Academic.
-git submodule update --remote --merge
+# Update Submodules
+git submodule update --init
